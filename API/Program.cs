70c30249a882103700a27580d9abf2df285b049d
Nicolae <Nicolae@NICU>
2021-11-24 10:51:01 +0200

    create react app

--- API/Program.cs
+++ API/Program.cs
@@ -31,7 +31,6 @@ namespace API
             }
             catch (Exception ex)
             {
-
                 var logger = services.GetRequiredService<ILogger<Program>>();
                 logger.LogError(ex, "An error occured during migration");
             }
