ba7bd2203e524d29a9dfdeba4ed8145d5c3d3167
Nicolae <nicolae.peptea@gmail.com>
2021-12-22 14:50:39 +0200

    create login endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using API.DTO;
+using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = _userManager.FindByEmailAsync(loginDto.Email).Result;
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (result.Succeeded)
+            {
+                return new UserDto
+                {
+                    DisplayName = user.DisplayName,
+                    Image = null,
+                    Token = "This will be a token",
+                    Username = user.UserName,
+                };
+            }
+
+            return Unauthorized();
+        }
+    }
+}
