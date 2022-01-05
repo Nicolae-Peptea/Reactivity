908fa73987b3f004a9c50a326694cde9543340d7
Nicolae <nicolae.peptea@gmail.com>
2022-01-05 12:09:01 +0200

    add photo logic

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -39,7 +39,10 @@ namespace API.Extensions
 
             services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            
             services.AddScoped<IUserAccessor, UserAccessor>();
+            services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            
             services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
 
             return services;
