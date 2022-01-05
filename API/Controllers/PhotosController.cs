6531b3f5f6feda3f1e3a369cf2d5b6dc504f8667
Nicolae <nicolae.peptea@gmail.com>
2022-01-05 13:34:29 +0200

    add photos controller

---
+++ API/Controllers/PhotosController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Photos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class PhotosController : BaseApiController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromForm] Add.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
+        }
+    }
+}
