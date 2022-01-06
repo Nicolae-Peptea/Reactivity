9e9c81360261a3e72c859fe97018d981cd72c42b
Nicolae <nicolae.peptea@gmail.com>
2022-01-06 12:48:54 +0200

    update profile details with actual data

--- client-app/src/Features/Profiles/ProfilePage.tsx
+++ client-app/src/Features/Profiles/ProfilePage.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import { observer } from "mobx-react-lite";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
+import LoadingComponent from "../../App/Layout/LoadingComponents";
+import { useStore } from "../../App/Stores/store";
 import ProfileContent from "./ProfileContent";
 import ProfileHeader from "./ProfileHeader";
 
-export default function ProfilePage() {
+export default observer (function ProfilePage() {
+    const {username} = useParams<{username: string}>();
+    const {profileStore} = useStore();
+    const {loadProfile, loadingProfile, profile} = profileStore;
+
+    useEffect(() => {
+        loadProfile(username);
+    }, [loadProfile, username])
+
+    if (loadingProfile) {
+        return <LoadingComponent content="Loading profile..." />
+    }
+
     return (
         <Grid>
             <Grid.Column width={16}>
-                <ProfileHeader />
+                {profile &&
+                <ProfileHeader profile={profile} />}
                 <ProfileContent />
             </Grid.Column>
         </Grid>
     )
-}
+})
