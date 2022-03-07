0c45244870845291ba3ce0ad70c3b8427f69017f
Nicolae <nicolae.peptea@gmail.com>
2022-03-07 20:08:30 +0200

    fixed syntax error

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -51,6 +51,7 @@ namespace API.Extensions
 
                             return Task.CompletedTask;
                         }
+                    };
                 });
 
             services.AddAuthorization(opt =>
