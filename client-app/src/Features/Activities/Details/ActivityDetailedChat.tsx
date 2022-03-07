e49284b8e23254554544b7945faba31acdbe53d7
Nicolae <nicolae.peptea@gmail.com>
2022-03-07 22:19:31 +0200

    solve UTC date format

--- client-app/src/Features/Activities/Details/ActivityDetailedChat.tsx
+++ client-app/src/Features/Activities/Details/ActivityDetailedChat.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import {Segment, Header, Comment, Loader} from 'semantic-ui-react';
 import { useStore } from '../../../App/Stores/store';
 import * as Yup from 'yup';
+import { formatDistanceToNow } from 'date-fns';
 
 interface Props {
     activityId: string;
@@ -44,7 +45,7 @@ export default observer(function ActivityDetailedChat({activityId}: Props) {
                                 <Comment.Content>
                                     <Comment.Author as={Link} to={`/profiles/${comment.username}`}>{comment.displayName}</Comment.Author>
                                     <Comment.Metadata>
-                                        <div>{comment.createdAt}</div>
+                                        <div>{formatDistanceToNow(comment.createdAt)} ago</div>
                                     </Comment.Metadata>
                                     <Comment.Text style={{whiteSpace: 'pre-wrap'}}>{comment.body}</Comment.Text>
                                 </Comment.Content>
