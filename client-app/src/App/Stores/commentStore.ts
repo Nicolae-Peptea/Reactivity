e2a1d1a0ffaefc77244236cf20a5bf46ed46bb34
Nicolae <nicolae.peptea@gmail.com>
2022-03-14 22:55:35 +0200

    delete comment

--- client-app/src/App/Stores/commentStore.ts
+++ client-app/src/App/Stores/commentStore.ts
@@ -36,7 +36,7 @@ export default class CommentStore {
             this.hubConnection.on("ReceiveComment", (comment: ChatComment) => {
                 runInAction(() => {
                     comment.createdAt = new Date(comment.createdAt);
-                    this.comments.unshift(comment); // work with a queue instead
+                    this.comments.unshift(comment);
                 });
             })
         }
