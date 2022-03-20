7d23c3b3f19d0a50a33dd19e9f53530c460ee8bd
Nicolae <nicolae.peptea@gmail.com>
2022-03-20 14:36:35 +0200

    create follow button

--- client-app/src/App/Stores/activityStore.ts
+++ client-app/src/App/Stores/activityStore.ts
@@ -175,6 +175,17 @@ export default class ActivityStore {
         }
     }
 
+    updateAttendeeFollowing = (username: string) => {
+        this.activityRegistry.forEach(activity => {
+            activity.attendees.forEach(attendee => {
+                if (attendee.username === username) {
+                    attendee.following ? attendee.followersCount-- : attendee.followersCount++;
+                    attendee.following = !attendee.following;
+                }
+            })
+        })
+    }
+
     cancelActivityToggle = async () => {
         this.loading = true;
         
