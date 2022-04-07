bae1718f83fb06ba166c360491e09930804f8dfa
Nicolae <nicolae.peptea@gmail.com>
2022-04-07 22:26:27 +0300

    create endpoint to send to the client the Activities for each user

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -17,5 +17,15 @@ namespace API.Controllers
         {
             return HandleResult(await Mediator.Send(new Edit.Command { Profile = profile }));
         }
+
+        [HttpGet("{username}/activities")]
+        public async Task<IActionResult> GetUserActivities(string userName, string predicate)
+        {
+            return HandleResult(await Mediator.Send(new ListActivities.Query
+            {
+                Username = userName,
+                Predicate = predicate
+            }));
+        }
     }
 }
