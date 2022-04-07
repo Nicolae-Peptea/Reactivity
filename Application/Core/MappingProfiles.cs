83d7194685350e5b8c0c331f18a347ed8efbda92
Nicolae <nicolae.peptea@gmail.com>
2022-04-07 22:21:09 +0300

    create mapping profiles for userActivityDto

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -6,7 +6,7 @@ using System.Linq;
 
 namespace Application.Core
 {
-    public class MappingProfiles : Profile
+    public class MappingProfiles : AutoMapper.Profile
     {
         public MappingProfiles()
         {
@@ -41,6 +41,14 @@ namespace Application.Core
                 .ForMember(d => d.DisplayName, opt => opt.MapFrom(s => s.Author.DisplayName))
                 .ForMember(d => d.Username, opt => opt.MapFrom(s => s.Author.UserName))
                 .ForMember(d => d.Image, o => o.MapFrom(s => s.Author.Photos.FirstOrDefault(x => x.IsMain).Url));
+
+            CreateMap<ActivityAttendee, Profiles.UserActivityDto>()
+                .ForMember(d => d.Id, o => o.MapFrom(s => s.Activity.Id))
+                .ForMember(d => d.Date, o => o.MapFrom(s => s.Activity.Date))
+                .ForMember(d => d.Title, o => o.MapFrom(s => s.Activity.Title))
+                .ForMember(d => d.Category, o => o.MapFrom(s => s.Activity.Category))
+                .ForMember(d => d.HostUsername, o => o.MapFrom(s => 
+                    s.Activity.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
         }
     }
 }
