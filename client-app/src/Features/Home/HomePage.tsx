1d2275f5e8ad1ddfb17d53e46015e3f928d190f7
Nicolae <nicolae.peptea@gmail.com>
2022-04-27 22:04:53 +0300

    remove useless breaks

--- client-app/src/Features/Home/HomePage.tsx
+++ client-app/src/Features/Home/HomePage.tsx
@@ -36,8 +36,6 @@ export default observer (function HomePage(){
                             />
                         </>
                     )}
-                    
-                  
                 </Container>
             </Segment>
         </Fragment>
