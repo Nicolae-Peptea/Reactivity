aaadacabf098561b01feaeee1cb515f08795f331
Nicolae <nicolae.peptea@gmail.com>
2022-04-27 21:44:29 +0300

    login with facebook fro backend

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,7 +5,11 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -19,13 +23,20 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
+        private readonly IConfiguration _config;
+        private readonly HttpClient _httpClient;
 
         public AccountController(UserManager<AppUser> userManager,
-            SignInManager<AppUser> signInManager, ITokenService tokenService)
+            SignInManager<AppUser> signInManager, ITokenService tokenService, IConfiguration config)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _tokenService = tokenService;
+            _config = config;
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new System.Uri("https://graph.facebook.com")
+            };
         }
 
         [HttpPost("login")]
@@ -87,6 +98,68 @@ namespace API.Controllers
         {
             AppUser user = await _userManager.Users.Include(p => p.Photos).
                 FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
+
+            return CreateUserDto(user);
+        }
+
+        [HttpPost("fbLogin")]
+        public async Task<ActionResult<UserDto>> FacebookLogin(string accessToken)
+        {
+            string fbVerifyKey = _config["Facebook:AppId"] + "|" + _config["Facebook:AppSecret"];
+            
+            HttpResponseMessage verifyToken = await _httpClient
+                .GetAsync($"debug_token?input_token={accessToken}&access_token={fbVerifyKey}");
+
+            if (!verifyToken.IsSuccessStatusCode)
+            {
+                return Unauthorized();
+            }
+
+            string fbUrl = $"me?access_token={accessToken}&fields=name,email,picture.width(100).height(100)";
+
+            HttpResponseMessage response = await _httpClient.GetAsync(fbUrl);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return Unauthorized();
+            }
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            dynamic fbInfo = JsonConvert.DeserializeObject<dynamic>(content);
+
+            string username = (string)fbInfo.id;
+
+            AppUser user = await _userManager.Users.Include(p => p.Photos)
+                .FirstOrDefaultAsync(x => x.UserName == username);
+
+            if (user != null)
+            {
+                return CreateUserDto(user);
+            }
+
+            user = new AppUser
+            {
+                DisplayName = (string)fbInfo.name,
+                Email = (string)fbInfo.email,
+                UserName = (string)fbInfo.id,
+                Photos = new List<Photo> 
+                { 
+                    new Photo 
+                    { 
+                        Id = $"fb_{(string)fbInfo.id}",
+                        Url=(string)fbInfo.picture.data.url,
+                        IsMain = true
+                    } 
+                }
+            };
+
+            var result = await _userManager.CreateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Problem Creating user account");
+            }
 
             return CreateUserDto(user);
         }
